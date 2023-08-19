aaaea4ec7095ac764f6e1f3a93e2b44bda0a3195
ZeynepBaglars <zeynepbaglars@gmail.com>
2023-08-19 22:46:08 +0300

    deneme için yapildi

--- src/US6_MesajKutusuKontrolu_Zeynep/denemeAmacli.java
+++ src/US6_MesajKutusuKontrolu_Zeynep/denemeAmacli.java
@@ -3,5 +3,6 @@ package US6_MesajKutusuKontrolu_Zeynep;
 public class denemeAmacli {
     public static void main(String[] args) {
 //        denemeAmacli yapildi
+        //herhangi bir sorun yok
     }
 }
