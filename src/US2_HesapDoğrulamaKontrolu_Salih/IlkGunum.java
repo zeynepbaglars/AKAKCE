3f3b33feba7d6df97af97b16395b869b8c57f480
Mutlumuer <mutluersalih.01@gmail.com>
2023-08-20 14:07:28 +0100

    selenium kütüphaneye eklendi ve chrome driver nesnesi oluşturuldu

--- src/US2_HesapDoğrulamaKontrolu_Salih/IlkGunum.java
+++ src/US2_HesapDoğrulamaKontrolu_Salih/IlkGunum.java
@@ -1,10 +1,12 @@
 package US2_HesapDoğrulamaKontrolu_Salih;
 
 import Utlity.BaseDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IlkGunum  {
     public static void main(String[] args) {
-        WebDiriver driver= new Chrom
+        WebDriver driver=new ChromeDriver();
     }
 
 
